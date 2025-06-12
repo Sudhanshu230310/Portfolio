7eb1dd6bf1da7674126ca093b7b5691fd802a7c6
Sudhanshu Gaur <gaurs8688@gmail.com>
2025-06-12 23:38:51 +0530

    page.tsx changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,36 @@
-"use client"
-import Image from "next/image";
-import {motion} from "motion/react"
+"use client";
+import { motion } from "motion/react";
 import NavBar from "@/Components/Navbar";
 import Main from "@/Components/Main";
-import AboutMe from "@/Components/About"
+import AboutMe from "@/Components/About";
 import Skills from "@/Components/Skill";
 import Projects from "@/Components/Project";
 import Contact from "@/Components/Contact";
+
 export default function Home() {
-  return (<>
-    <motion.div className="w-screen h-screen bg-black flex justify-center items-center" initial={{visibility:"visible"}} animate={{display:"none", transition:{duration:2,ease:"easeInOut"}}}>
-      <motion.div className="md:text-3xl text-xl font-semibold text-white" initial={{y:-20,opacity:0}} animate={{y:0,opacity:1,transition:{delay:0.5,duration:0.5}}}>Full Stack Developer | DevOps </motion.div>
-    </motion.div>
-    <motion.div initial={{display:"none"}} animate={{display:"block" , transition:{delay:2}}}>
-      <NavBar/>
-      <Main/>
-      <AboutMe/>
-      <Skills/>
-      <Projects/>
-      <Contact/>
-    </motion.div>
-  </>);
+  return (
+    <>
+      <motion.div
+        className="w-screen h-screen bg-black flex justify-center items-center"
+        initial={{ visibility: "visible" }}
+        animate={{ display: "none", transition: { duration: 2, ease: "easeInOut" } }}
+      >
+        <motion.div
+          className="md:text-3xl text-xl font-semibold text-white"
+          initial={{ y: -20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { delay: 0.5, duration: 0.5 } }}
+        >
+          Full Stack Developer | DevOps
+        </motion.div>
+      </motion.div>
+      <motion.div initial={{ display: "none" }} animate={{ display: "block", transition: { delay: 2 } }}>
+        <NavBar />
+        <Main />
+        <AboutMe />
+        <Skills />
+        <Projects />
+        <Contact />
+      </motion.div>
+    </>
+  );
 }
