e70fb3cec213cf5f7d1b477473cc96526cfceefa
Sudhanshu Gaur <gaurs8688@gmail.com>
2025-06-14 16:42:32 +0530

    change next config

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  basePath: '/Portfolio',
+  assetPrefix: '/Portfolio/',
+  images: {
+    unoptimized: true,
+  },
+}
+
 export default nextConfig;
